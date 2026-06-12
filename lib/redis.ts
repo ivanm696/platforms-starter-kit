f2170ef061f58e5612ee15a497f3a1ee15d56ab1
ivanm696 <59553806+ivanm696@users.noreply.github.com>
2026-06-12 09:54:05 +0300

    fix(security): Potential exposure of sensitive environment variables

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,1 @@
-import { Redis } from '@upstash/redis';
-
-export const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN
-});
+if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) { throw new Error('Redis connection variables are not set'); }
